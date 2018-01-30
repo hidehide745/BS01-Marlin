c50782c7d147b90e7030fd7d56e313037753c489
hammerhead <postman.th@gmail.com>
2018-01-31 00:21:26 +0900

    1st headと2nd headのオフセットを修正 温度センサのテーブルを入れ替え driver基板冷却用ファンコントロールピンを5にセット E0 温度に連動したファンのピンを11にセット E1 温度に連動したファンのピンを6にセット 造形物冷却を4番ピンで実施できることを確認

--- Marlin-Development/Marlin/Configuration_adv.h
+++ Marlin-Development/Marlin/Configuration_adv.h
@@ -69,8 +69,8 @@
 //This is for controlling a fan to cool down the stepper drivers
 //it will turn on when any driver is enabled
 //and turn off after the set amount of seconds from last driver being disabled again
-#define CONTROLLERFAN_PIN -1 //Pin used for the fan to cool controller (-1 to disable)
-#define CONTROLLERFAN_SECS 60 //How many seconds, after all motors were disabled, the fan should run
+#define CONTROLLERFAN_PIN  5 //Pin used for the fan to cool controller (-1 to disable)
+#define CONTROLLERFAN_SECS 120 //How many seconds, after all motors were disabled, the fan should run
 #define CONTROLLERFAN_SPEED 255  // == full speed
 
 // When first starting the main fan, run it at full speed for the
@@ -85,8 +85,8 @@
 // extruder temperature is above/below EXTRUDER_AUTO_FAN_TEMPERATURE.
 // Multiple extruders can be assigned to the same pin in which case
 // the fan will turn on when any selected extruder is above the threshold.
-#define EXTRUDER_0_AUTO_FAN_PIN -1
-#define EXTRUDER_1_AUTO_FAN_PIN -1
+#define EXTRUDER_0_AUTO_FAN_PIN 11
+#define EXTRUDER_1_AUTO_FAN_PIN  6
 #define EXTRUDER_2_AUTO_FAN_PIN -1
 #define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
